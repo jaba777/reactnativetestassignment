10d4778624eb697b528677d769f0fcd74512eb8d
jaba777 <96587911+jaba777@users.noreply.github.com>
2024-04-19 09:52:47 +0000

    first commit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
+
 export const Home = () => {
+
+  const firstSolution = () => {
+    const arr = [1, 2, 3, 4, 2, 3];
+    let text = ""
+    for (let i = 0; i < arr.length; i++) {
+      if (i === 1) {
+
+        text += arr[1]
+        text += arr[0]
+      }
+      if (i % 2 === 1 && i !== 1) {
+
+        text += arr[i]
+        text += arr[i - 1]
+      }
+    }
+    return text
+  }
+
+  function secondSolution(arr) {
+    let maxDiff = 0;
+
+    arr.forEach((num, index) => {
+      arr.slice(index + 1).forEach(nextNum => {
+        if (nextNum > num) {
+          maxDiff = Math.max(maxDiff, nextNum - num);
+        }
+      });
+    });
+
+    return maxDiff;
+  }
+
+  function thirdSolution(str1, str2) {
+    if (str1.length !== str2.length) {
+      return false; // If lengths are different, rotation is not possible
+    }
+
+    // Double the first string to create a circular effect
+    let doubledStr1 = str1 + str1;
+
+    // Check if the second string is a substring of the doubled first string
+    return doubledStr1.includes(str2);
+  }
+
   return (
     <div className='App'>
       <header className='App-header'>
--- src/routes/List.jsx
+++ src/routes/List.jsx
@@ -1,5 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Array_1 from '../data/Array_1.json';
+import Array_2 from "../data/Array_2.json";
 
 export default function List() {
-  return <div>list</div>
+  const [mergedArray, setMergedArray] = useState([]);
+
+
+  // const [mixJson, setMixJson] = useState([]);
+
+  
+
+
+  useEffect(() => {
+    const mergeArrays = (array1, array2) => {
+      const merged = [...array1];
+      for (const item of array2) {
+        if (!merged.some(existingItem => existingItem.id === item.id)) {
+          merged.push(item);
+        }
+      }
+      return merged;
+    };
+
+    setMergedArray(mergeArrays(Array_1, Array_2));
+  }, []);
+
+
+  return (
+    <div>
+      {mergedArray.map(item => (
+        <div key={item.id}>
+          <p>{item.name}</p>
+          <p>{item.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
